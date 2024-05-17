290a53ca99c7b38829780b57257bd7dbca585c23
KietSeng <kietseng078@gmail.com>
2024-05-17 19:23:40 +0700

    add file

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ var con = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database : process.env.DB_DATABASE
-});
+}); 
 
 con.connect(function(err) {
   if (err) throw err;
